8bf1abb5b2269b355d48bbd56a0a8fa0277feaa4
flata <havikoro2004@gmail.com>
2021-10-03 12:27:31 +0200

    Add gitignore

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1633439655;s:7:"authKey";s:64:"KrHp~I$~7Yt0,.fDEQS%XMKe#z2?JW~}28jD3!e}n:A)D.B+#>9O[#Zr[Oz`eL.:";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:581;}
